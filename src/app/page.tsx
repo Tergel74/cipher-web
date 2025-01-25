88687416e2203d2f6b4f3d585df25691b0f5fdff
Tergel Bayarsaikhan <tergel7006@gmail.com>
2025-01-25 18:34:35 +0800

    pop up design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -142,7 +142,7 @@ export default function Home() {
                     </div>
 
                     {showCopyNotification && (
-                        <div className="fixed top-4 left-4 bg-popover text-popover-foreground px-4 py-2 rounded-md shadow-lg">
+                        <div className="fixed top-4 left-4 bg-primary text-white text-popover-foreground px-4 py-2 rounded-md shadow-lg">
                             Copied to clipboard!
                         </div>
                     )}
